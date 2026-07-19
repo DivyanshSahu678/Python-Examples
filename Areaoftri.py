56c2d1d89529f2aa19afe5368b675600e8787e3e
DivyanshSahu678 <sahudivyansh965@gmail.com>
2026-07-19 22:23:03 +0530

    Program to find quadratic equation

--- Areaoftri.py
+++ Areaoftri.py
@@ -4,4 +4,4 @@ height = float(input("Enter the height of the triangle: "))
 print("Base is: ", base, "\n" "Height is: ", height)
 
 area = 0.5 * base * height
-print("Area of the triangle is: ", area)
+print("Area of the triangle is: ", area)       
